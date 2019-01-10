d9090e16cff797851ed77d60b9d3aab5e9fac825
Agustin Gonzalez Collins <agonzalezcollins@frsf.utn.edu.ar>
2019-01-09 22:38:25 -0300

    #06 Agregado de Aplicacion JDownload

---
+++ apps/JDownload/install_jdownload2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+echo "WEB: http://jdownloader.org/jdownloader2"
+wget https://mega.nz/#!LJ9FyK7b!t88t6YBo2Wm_ABkSO7GikxujDF5Hddng9bgDb8fwoJQ
+./JD2Setup_x64.sh
+rm JD2Setup_x64.sh
--- apps/Postman/install_postman.sh
+++ apps/Postman/install_postman.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "WEB: https://www.getpostman.com/downloads/"
 wget https://dl.pstmn.io/download/latest/linux64
 tar xzf Postman-linux-x64-*.tar.gz
 sudo mv Postman /opt/
