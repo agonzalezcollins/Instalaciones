e070816e4bd023608b135ce5eac26e612bef7c51
Agustin Gonzalez Collins <agonzalezcollins@frsf.utn.edu.ar>
2019-01-09 22:26:38 -0300

    #05 Agregado de Aplicacion GCM

---
+++ apps/Gnome Connection Manager/install_gcm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Ver WEB: http://kuthulu.com/gcm/?module=download"
+wget http://kuthulu.com/gcm/gnome-connection-manager_1.1.0_all.deb
+sudo dpkg -i gnome-connection-manager_1.1.0_all.deb
